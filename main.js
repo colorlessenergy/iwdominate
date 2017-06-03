53fb4de5e87feb3fded24d76d5e0372f7ac21c23
colorlessenergy <brianmunozdev@gmail.com>
2017-06-03 08:00:08 -0700

    finished responsive home page

---
+++ main.js
@@ -0,0 +1,6 @@
+var responsivbutton = document.getElementById("button");
+var responsivenav = document.getElementById("rs");
+
+responsivbutton.addEventListener("click", function () {
+  responsivenav.classList.toggle("responsive-nav")
+});
